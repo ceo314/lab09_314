2e78e0d9a4b2c081c6eb991c425598e987919e12
CEO_314 <100899535+ceo314@users.noreply.github.com>
2026-03-17 10:12:03 +0500

    Создал файл Node.java с решением пятого задания

---
+++ 09_5/Node.java
@@ -0,0 +1,15 @@
+package lab09_5;
+
+//Узел однонаправленного списка
+
+public class Node {
+    int value; //данные
+    Node next; //ссылка на следующий узел
+    Node prev; //ссылка на предыдущий узел
+
+    Node(int value){
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
+}
